45f6e38ebfe969cdf8ceac92313486b500943f4c
Tomas Silva <silvamolinatomas@gmail.com>
2025-06-16 20:39:24 -0400

    dashboard try catch

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,8 +8,13 @@ import { openDB } from "@/lib/sqlite";
 
 
 export default async function Page() {
+  let data
+  try {
   const db = await openDB() 
-  const data = await db.all("select * from funerarias")
+  data = await db.all("select * from funerarias")
+  } catch (e) {
+    data = []
+  }
   console.log(data)
   return (
       <div style={{ display: 'flex'}}>
